24234692c3806787bc47c9697d0f60a2ffc13073
Ha1101 <145737231+Ha1101@users.noreply.github.com>
2024-07-14 09:15:18 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+print("Calculator")
+Notes:
+num1=int(input("Enter First Number: "))
+num2=int(input("Enter second Number:"))
+#Operations !
+print ("press + for addition \n press - for substraction \n press * for
+Multiplication \n press / for Division")
+Choice=(input ("Enter Your operation from above "))
+if Choice == '+' :
+print(num1+num2)
+elif Choice == '-' :
+print(num1-num2)
+elif Choice == '*' :
+print(num1*num2)
+elif Choice == '/' :
+print(num1/num2)
+else:
+print("Enter a Valid Operator")
